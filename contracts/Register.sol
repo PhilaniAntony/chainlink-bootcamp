0259a71ea3e2650ddfb004457ca8d0e0a33812ef
PhilaniAntony <philaniantonydlamini@gmail.com>
2024-10-10 23:24:10 +0200

    Initial commit

---
+++ contracts/Register.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+
+contract Register {
+
+    string private info;
+
+    function getinfo() public view returns(string memory) {
+        return info;
+    }
+
+    function setInfo(string memory _info) public {
+        info = _info;
+    }
+}
